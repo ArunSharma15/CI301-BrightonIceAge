9f3d5026e3ff3170aaae7ce97bfcad3bfc9e0f1c
ArunSharma15 <as988@brighton.ac.uk>
2018-02-12 11:30:25 +0000

    Added GUI library and simple button
    Introduced the GUI Library into the Index.html file and generated a simple button on the title screen; which once pressed will remove all assets on the screen.

--- ts/game.ts
+++ ts/game.ts
@@ -1,16 +1,18 @@
  /// <reference path="../node_modules/babylonjs/dist/preview release/babylon.d.ts" />import { Color3 } from "babylonjs";
 
- // Import class from other file.
+ // Import classes from other files.
 import { TitleScreen } from "./title";
+import * as GUI from 'babylonjs-gui';
+import { BabylonMessage } from "babylonjs";
 
 class Game {
     private canvas: HTMLCanvasElement;
     private engine: BABYLON.Engine;
     private scene: BABYLON.Scene;
-    private camera: BABYLON.FreeCamera;
-    private titlecamera: BABYLON.FollowCamera;
+    // dynamically assign because of free or follow cam.
+    private camera;
     private light: BABYLON.Light;
-    
+
     constructor(canvasElement : string) {
         // Have to case canvas to HTMLCanvasElement Type.
         this.canvas = <HTMLCanvasElement>document.getElementById(canvasElement);
@@ -26,13 +28,33 @@ class Game {
      * Setup and run the main title screen.
      */
     public runTitleScene():void{
-        let TS = new TitleScreen(this.engine);
+        let TS = new TitleScreen(this.engine,true);
         TS.setupScene();
-        this.titlecamera = TS.getCamera();
+        this.camera = TS.getCamera();
         this.scene = TS.getScene();
         this.light = TS.getLight();
     }
 
+    public GenerateUI():void{
+        let advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI",true,this.scene);
+        let panel = new BABYLON.GUI.StackPanel();
+        panel.width = 0.1;
+        panel.height = 0.1;
+        panel.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        advancedTexture.addControl(panel);
+        let button = BABYLON.GUI.Button.CreateSimpleButton("b1","Enter");
+        button.background = "grey";
+        panel.addControl(button);
+        button.onPointerDownObservable.add(() => this.scene.getMeshByID("ground").dispose());
+        button.onPointerDownObservable.add(() => this.scene.getMeshByID("title3D").dispose());
+        button.onPointerDownObservable.add(() => this.scene.getMeshByID("fountain").dispose());
+        button.onPointerDownObservable.add(() => alert("titleHasbeenSKipped"));
+    }
+
+
+    /**
+     * Render loop.
+     */
     run() : void {
         this.engine.runRenderLoop(()=> {
             this.scene.render();
@@ -45,6 +67,10 @@ class Game {
 
   // Title Screen.
   game.runTitleScene();
+  
+  game.GenerateUI();
 
   // start animation
   game.run();
+
+  
--- ts/title.ts
+++ ts/title.ts
@@ -1,3 +1,5 @@
+import { BabylonMessage } from "babylonjs";
+
 /**
  * Class to setup and load the 'Title Screen'
  * Create spinning text and particle effects.
@@ -8,16 +10,24 @@ export class TitleScreen{
     private engine: BABYLON.Engine;
     private scene: BABYLON.Scene;
     private camera: BABYLON.FollowCamera;
+    private devCamera: BABYLON.FreeCamera;
     private light: BABYLON.Light;
+    private devCameraEnabled: Boolean;
+    
+    //private meshLibrary:BABYLON.AbstractMesh;
 
     // Pass in engine from main class
     // Otherwise we would have to address HTML (element) canvasElement from here.
-    constructor(main_engine){this.engine = main_engine;}
+    constructor(main_engine:BABYLON.Engine,devCameraEnable:Boolean){
+        this.engine = main_engine; 
+        this.devCameraEnabled = devCameraEnable;
+    }
 
     // Use method to return objects once scene has been constructed.
     public getScene():BABYLON.Scene        {return this.scene;}
     public getCamera():BABYLON.FollowCamera{return this.camera;}
     public getLight():BABYLON.Light        {return this.light};
+    public getIsSceneSkipped():Boolean     {return this.isSceneSkipped};
 
     /**
      * Setup an hemispheric light.
@@ -73,6 +83,15 @@ export class TitleScreen{
         this.camera.rotation.x = -0.1;    
     }
 
+    private setupDevCamera():void{
+        this.devCamera = new BABYLON.FreeCamera('devCamera', new BABYLON.Vector3(60, 20,80), this.scene);
+    
+        this.devCamera.setTarget(BABYLON.Vector3.Zero());
+
+        this.devCamera.attachControl(this.engine.getRenderingCanvas(), false);
+    }
+    
+
     /**
      * Load in 3D Mesh with accompanying animation.
      * 3D Mesh is however stored in .babylon format generated from 3DS Max Exporter.
@@ -85,6 +104,7 @@ export class TitleScreen{
         var meshTask = assetsManager.addMeshTask("titleTask","","models/","title_text.babylon");
         // Mesh will be positioned, scaled then animation will be begin if loading is succesful.
         meshTask.onSuccess = function(task){
+            task.loadedMeshes[0].id ="title3D";
             task.loadedMeshes[0].position = new BABYLON.Vector3(35,20,50);
             task.loadedMeshes[0].scaling = new BABYLON.Vector3(0.5,0.5,0.5);
             task.loadedMeshes[0].beginAnimation("title_Animation",true);
@@ -94,19 +114,30 @@ export class TitleScreen{
         meshTask.onError = function(task){
             console.log("Error loading mesh");
         }
+        
         // Start tasks!
         assetsManager.load();
+        
     }
+
+    
+
 
     /**
      * Call methods which populate the Babylon Camera,Engine,Scene and Light Objects.
      */
     public setupScene():void{
         this.setupLights();
+        // Call either regular fixed camera or dev camera depending on boolean.
+        if(this.devCameraEnabled){
+            this.setupDevCamera();
+        }else{
         this.setupCamera();
+        }
         this.setupTerrain();
         this.setupParticleEffects();
         this.loadAssets();
+        
     }
 
 }
